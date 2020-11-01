413d4e968bc83f6fb082383d4969db409123f545
Alin Movileanu <translin.mondo@yahoo.com>
2020-11-01 14:13:46 +0200

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Movileanu_Alin_Lab3.Models;
+using Microsoft.EntityFrameworkCore;
+using Movileanu_Alin_Lab3.Data;
+using Movileanu_Alin_Lab3.Models.LibraryViewModels;
+
+namespace Movileanu_Alin_Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> Statistics ()
+            {
+                IQueryable<OrderGroup> data =
+                from order in _context.Orders
+                group order by order.OrderDate into dateGroup
+                select new OrderGroup()
+                {
+                    OrderDate = dateGroup.Key,
+                    BookCount = dateGroup.Count()
+                };
+                return View(await data.AsNoTracking().ToListAsync());
+            }
+        
+        }
+    }
+
---
+++ Models/LibraryViewModels/OrderGroup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Movileanu_Alin_Lab3.Models.LibraryViewModels
+{
+    public class OrderGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? OrderDate { get; set; }
+        public int BookCount { get; set; }
+    }
+}
